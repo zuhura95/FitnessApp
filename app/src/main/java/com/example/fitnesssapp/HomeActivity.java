5898e270e04754977a375660968a29bd107a4bff
zuhura95 <contact.zuhura@gmail.com>
2019-10-22 14:06:47 +0300

    keystores added

--- app/src/main/java/com/example/fitnesssapp/HomeActivity.java
+++ app/src/main/java/com/example/fitnesssapp/HomeActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 
 
@@ -16,6 +17,12 @@ import android.view.View;
 
 
 import com.github.lzyzsd.circleprogress.ArcProgress;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
@@ -77,6 +84,7 @@ import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -144,7 +152,14 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         //set the steps counter maximum value to goal set
         stepsCounter.setMax(sharedPreferences.getInt("Goal",5000));
 
+        BarChart chart = (BarChart) findViewById(R.id.chart);
 
+//        BarDataSet barDataSet = new BarDataSet(getDataSet(), "SETS");
+//        BarData data = new BarData((IBarDataSet) getXAxisValues(), barDataSet);
+//        chart.setData(data);
+////        chart.setDescription();
+//        chart.animateXY(2000, 2000);
+//        chart.invalidate();
 //        String name = sharedPreferences.getString("FirstName",null);
 
 
@@ -413,6 +428,59 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+    private ArrayList getDataSet() {
+        ArrayList dataSets = null;
+
+        ArrayList valueSet1 = new ArrayList();
+        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+        valueSet1.add(v1e6);
+
+        ArrayList valueSet2 = new ArrayList();
+        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
+        valueSet2.add(v2e1);
+        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
+        valueSet2.add(v2e2);
+        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
+        valueSet2.add(v2e3);
+        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
+        valueSet2.add(v2e4);
+        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
+        valueSet2.add(v2e5);
+        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
+        valueSet2.add(v2e6);
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
+        barDataSet1.setColor(Color.rgb(0, 155, 0));
+        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
+        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        dataSets = new ArrayList();
+        dataSets.add(barDataSet1);
+        dataSets.add(barDataSet2);
+        return dataSets;
+    }
+
+    private ArrayList getXAxisValues() {
+        ArrayList xAxis = new ArrayList();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        return xAxis;
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -426,18 +494,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                     Log.d(TAG, "Field: " + field.getName());
                     Log.d(TAG, "Value: " + dataPoint.getValue(field));
 
-                    if (field.getName().equals("steps")) {
-                       int currentSteps = stepsCounter.getProgress();
-                        currentSteps = currentSteps + dataPoint.getValue(field).asInt();
-                        //userSteps.setText(Integer.toString(currentSteps));
-                        stepsCounter.setProgress(currentSteps);
-                        double steps = Double.parseDouble(String.valueOf(currentSteps));
-                        double value =( steps / sharedPreferences.getInt("Goal",5000)) * 100;
-//
-                        stepsPercentage.setText(value+"% OF GOAL "+ (sharedPreferences.getInt("Goal",5000)));
 
-
-                    }
                 }
             }
         };
